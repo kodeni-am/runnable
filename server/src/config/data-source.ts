ebe3cfc57e534aa17dd132f244a285912c5dea5f
Ara Asryan <araasr@gmail.com>
2026-03-04 17:49:53 +0400

    fix: register domain redirect migration

--- server/src/config/data-source.ts
+++ server/src/config/data-source.ts
@@ -7,6 +7,7 @@ import { GithubRepo } from '../entities/GithubRepo';
 import { CustomDomain } from '../entities/CustomDomain';
 import { AppSettings } from '../entities/AppSettings';
 import { InitialSchema1709520000000 } from '../migrations/1709520000000-InitialSchema';
+import { AddDomainRedirectTarget1772633000000 } from '../migration/1772633000000-AddDomainRedirectTarget';
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
@@ -19,6 +20,6 @@ export const AppDataSource = new DataSource({
     migrationsRun: true, // Auto-run pending migrations on startup
     logging: config.nodeEnv === 'development',
     entities: [User, Project, GithubRepo, CustomDomain, AppSettings],
-    migrations: [InitialSchema1709520000000],
+    migrations: [InitialSchema1709520000000, AddDomainRedirectTarget1772633000000],
     subscribers: [],
 });
